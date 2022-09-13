3e928eb0cecf0a4636c97843f1c9e835ec93e543
brianf203 <briancf203@gmail.com>
2022-09-13 19:49:07 -0400

    initial

--- Lab3/src/UnitConverter.java
+++ Lab3/src/UnitConverter.java
@@ -11,25 +11,25 @@ public class UnitConverter {
 			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 0.393701) + " in"));
 		}
 		else if (unit.equalsIgnoreCase("in")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 2.54) + " cm"));	
+			JOptionPane.showMessageDialog(null, (value + "") + "in = " + ((value * 2.54) + " cm"));	
 		}
 		else if (unit.equalsIgnoreCase("yd")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 0.9144) + " m"));
+			JOptionPane.showMessageDialog(null, (value + "") + "yd = " + ((value * 0.9144) + " m"));
 		}
 		else if (unit.equalsIgnoreCase("m")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 1.09361) + " yd"));
+			JOptionPane.showMessageDialog(null, (value + "") + "m = " + ((value * 1.09361) + " yd"));
 		}
 		else if (unit.equalsIgnoreCase("oz")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 28.3495) + " gm"));
+			JOptionPane.showMessageDialog(null, (value + "") + "oz = " + ((value * 28.3495) + " gm"));
 		}
 		else if (unit.equalsIgnoreCase("gm")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 0.035274) + " oz"));
+			JOptionPane.showMessageDialog(null, (value + "") + "gm = " + ((value * 0.035274) + " oz"));
 		}
 		else if (unit.equalsIgnoreCase("lb")) {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 0.453592) + " kg"));
+			JOptionPane.showMessageDialog(null, (value + "") + "lb = " + ((value * 0.453592) + " kg"));
 		}
 		else {
-			JOptionPane.showMessageDialog(null, (value + "") + "cm = " + ((value * 2.20462) + " lb"));
+			JOptionPane.showMessageDialog(null, (value + "") + "kg = " + ((value * 2.20462) + " lb"));
 		}
 	}
 
